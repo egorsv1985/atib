2d6a3ae48b0696602e88d8e0357529dae5432f09
egorsv1985 <egorvs2009@gmail.com>
2024-02-22 17:00:19 +0300

    added start section team

--- src/js/components/slider-portfolio-page.js
+++ src/js/components/slider-portfolio-page.js
@@ -13,19 +13,19 @@ $(document).ready(function () {
 
 	// Инициализация слайдеров
 	$(
-		'.page-branding__slider, .page-development__slider, .page-promotion__slider'
+		'.page-branding__slider, .page-development__slider, .page-promotion__slider, team__slider'
 	).slick(settings)
 
 	// Обработчик события изменения размера окна
 	$(window).on('resize', function () {
 		if ($(window).width() > 640) {
 			$(
-				'.page-branding__slider, .page-development__slider, .page-promotion__slider'
+				'.page-branding__slider, .page-development__slider, .page-promotion__slider, team__slider'
 			).slick('unslick')
 			return
 		}
 		$(
-			'.page-branding__slider, .page-development__slider, .page-promotion__slider'
+			'.page-branding__slider, .page-development__slider, .page-promotion__slider, team__slider'
 		)
 			.not('.slick-initialized')
 			.slick(settings)
