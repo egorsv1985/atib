e689b8401c8be7cca5d593409938da5189c9b305
egorsv1985 <egorvs2009@gmail.com>
2024-07-12 16:36:50 +0300

    fix

--- bitrix/templates/atib/components/bitrix/news.list/tariffs/template.php
+++ bitrix/templates/atib/components/bitrix/news.list/tariffs/template.php
@@ -30,9 +30,9 @@ $this->setFrameMode(true);
 			индивидуально, в зависимости от объёмов, сложности и сроков.</p>
 	</div>
 	<div class="grid grid-cols-3 gap-7 mb-11">
-		<div class="col-span-3 lg:col-span-1">
+		<div class="col-span-3 lg:col-span-1 lg:row-span-2 ">
 			<? foreach ($arResult["ITEMS"] as $key => $arItem) : ?>
-				<div class="tariff-detail  border bg-<?= $arItem["PROPERTIES"]["COLOR"]["VALUE"] ?>_500 border-<?= $arItem["PROPERTIES"]["COLOR"]["VALUE"] ?>_border  shadow-lg px-7 py-12" id="tariff-detail-<?= $arItem['ID']; ?>">
+				<div class="tariff-detail rounded-[10px]  border bg-<?= $arItem["PROPERTIES"]["COLOR"]["VALUE"] ?>_500 border-<?= $arItem["PROPERTIES"]["COLOR"]["VALUE"] ?>_border  shadow-lg px-7 py-12" id="tariff-detail-<?= $arItem['ID']; ?>">
 					<div class=" px-4 mb-6 py-1 text-sm leading-tight text-white border bg-gray_light border-gray_border rounded-[20px] inline-flex">
 						Детали по проекту
 					</div>
@@ -46,7 +46,7 @@ $this->setFrameMode(true);
 				</div>
 			<? endforeach; ?>
 		</div>
-		<div class="col-span-3 lg:col-span-2">
+		<div class="col-span-3 lg:col-span-2 -order-1 lg:order-0">
 			<div class="flex items-center justify-between mb-4">
 				<ul class="flex flex-wrap border-gray_border bg-gray_light rounded-[40px] -mb-px text-lg font-medium leading-tight text-center text-<?= $arItem["PROPERTIES"]["COLOR"]["VALUE"] ?>_border p-4 gap-2" id="tariffs-tab" data-tabs-toggle="#tariffs-tab-content" role="tablist">
 					<? foreach ($arResult["ITEMS"] as $key => $arItem) : ?>
@@ -55,15 +55,45 @@ $this->setFrameMode(true);
 						</li>
 					<? endforeach; ?>
 				</ul>
-				<a href="" class="flex gap-3">
-					<span class="flex items-center justify-center w-10 h-10 border rounded-full border-gray_border bg-gray_500"><img src="/images/icons/question.svg" alt="question" /></span>
+
+
+				<!-- Modal toggle -->
+				<button data-modal-target="question-modal" data-modal-toggle="question-modal" class="flex gap-3" type="button">
+					<span class="flex items-center justify-center w-10 h-10 border rounded-full border-gray_border bg-gray_500"><img src="<?= SITE_TEMPLATE_PATH ?>/images/icons/question.svg" alt="question" /></span>
 					<p class="leading-tight text-txt">
 						В чем отличие
 
 						данных CMS?
 					</p>
-				</a>
+				</button>
+				<? foreach ($arResult["ITEMS"] as $key => $arItem) : ?>
+					<!-- Main modal -->
+					<div id="question-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+						<div class="relative p-4 w-full max-w-2xl max-h-full">
+							<!-- Modal content -->
+							<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+								<!-- Modal header -->
+								<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+
+									<button type="button" class=" bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="question-modal">
+										<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+											<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+										</svg>
+
+									</button>
+								</div>
+								<!-- Modal body -->
+								<div class="p-4">
+									<?= $arItem['DETAIL_TEXT']; ?>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				<? endforeach; ?>
 			</div>
+		</div>
+		<div class="col-span-3 lg:col-span-2">
 			<div id="tariffs-tab-content">
 				<? foreach ($arResult["ITEMS"] as $key => $arItem) :
 					$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
@@ -106,16 +136,26 @@ $this->setFrameMode(true);
 <script>
 	$(document).ready(function() {
 		$('.tariff-tab').on('click', function() {
-			var targetId = $(this).data('tabs-target');
-			var targetDetailId = '#tariff-detail-' + targetId.split('-')[1];
+			// Устанавливаем aria-selected для всех вкладок в false
+			$('.tariff-tab').attr('aria-selected', 'false');
+			// Устанавливаем aria-selected для текущей вкладки в true
+			$(this).attr('aria-selected', 'true');
 
 			// Удаляем класс detail-active у всех блоков
 			$('.tariff-detail').removeClass('detail-active');
 
 			// Добавляем класс detail-active к соответствующему блоку
+			var targetId = $(this).data('tabs-target');
+			var targetDetailId = '#tariff-detail-' + targetId.split('-')[1];
 			$(targetDetailId).addClass('detail-active');
 		});
 
-		
+		// Устанавливаем первую вкладку активной по умолчанию
+		if ($('.tariff-tab').length > 0) {
+			$('.tariff-tab').first().click();
+		}
+
+
+
 	});
 </script>
